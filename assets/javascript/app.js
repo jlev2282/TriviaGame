b2edd71a642d383b95582583fb28b2a10461de27
John <jlev2282@gmail.com>
2017-03-17 20:33:40 -0400

    start page and some of the game deck array

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+gameDeck = [
+	{
+	category:"Bill and Ted's Excellent Adventure",
+	backImage: url('../images/billandted1.jpg'),
+	biopic: url(),
+	bio: "Two seemingly dumb teens struggle to prepare a historical presentation with the help of a time machine.",
+	quote:
+	form: billTedAd
+	};
+];
+
+billTedAd=[	
+	Dollars:
+	Plot1:
+	Plot2:
+	Plot3:];
+
+};
