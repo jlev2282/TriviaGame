61568322026dd3a6f4a9fd27b9d5500345378367
John <jlev2282@gmail.com>
2017-03-22 09:46:42 -0400

    added stage info and initial objects to pull from for bioingo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,44 @@
 $(document).ready(function (){
 gameDeck = [
 	{
-	category:"Bill and Ted's Excellent Adventure",
-	backImage: url('../images/billandted1.jpg'),
-	biopic: url(),
-	bio: "Two seemingly dumb teens struggle to prepare a historical presentation with the help of a time machine.",
-	quote:
-	form: billTedAd
-	};
+		category:"Bill and Ted's Excellent Adventure",
+		backImage: url('../images/billandted1.jpg'),
+		biopic: url('../images/billandtedbethoveen.jpg'),
+		bio: "Two seemingly dumb teens struggle to prepare a historical presentation with the help of a time machine.",
+		form: billTedAd
+	},
+
+	{
+		category: "Back to the Future",
+		backImage: url('../images/backtothefutureback.jpg'),
+		biopic: url('../images/backtothefuturebio.jpg')
+		biopic
+		form
+	},
+
+	{
+		category
+		backImage
+		biopic
+		biopic
+		form
+	}
 ];
 
+
 billTedAd=[	
+	q1 {
 	Dollars:
+		},
+	q2 {
 	Plot1:
+		},
+	q3 {
 	Plot2:
-	Plot3:];
+		},
+	q4 {
+	Plot3:
+		}
+	];
 
 };
